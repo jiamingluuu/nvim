a5e69fc6d70da57e5eec278fbd2649f2a2bd835d
Jiaming Lu <jiaming.lu.0124@gamil.com>
2022-11-22 16:48:02 -0500

    added markdowmpreview.lua

--- init.lua
+++ init.lua
@@ -3,9 +3,12 @@ require("core.options")
 require("core.keymaps")
 require("core.colorscheme")
 require("core.coc")
+
+-- plugin settings
 require("plugins.indent_blankline")
 require("plugins.comment")
 require("plugins.lualine")
 require("plugins.telescope")
 require("plugins.vimtex")
 require("plugins.virtcolumn")
+require("plugins.markdownpreview")
---
+++ lua/plugins/markdownpreview.lua
@@ -0,0 +1,5 @@
+local keymap = vim.keymap
+keymap.set("n", "<leader>md", ":MarkdownPreview<CR>")
+keymap.set("n", "<leader>ms", ":MarkdownPreviewStop<CR>")
+keymap.set("n", "<leader>mt", ":MarkdownPreviewToggle<CR>")
+
